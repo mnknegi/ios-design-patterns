5efb68889a770cdb37b5b2767a1f4320abeb68db
imayanknegi29 <imayanknegi29@gmail.com>
2025-05-03 09:43:29 +0530

    usecase of strategy pattern

--- behavioral-design-pattern/strategy/Strategy.playground/Contents.swift
+++ behavioral-design-pattern/strategy/Strategy.playground/Contents.swift
@@ -66,3 +66,20 @@ context.pay(amount: 200.0)
 
 context.setStrategy(ApplePayment())
 context.pay(amount: 300.0)
+
+/*
+ Benefits:
+ - Eliminates giant if-else or switch statements.
+ - Add new strategy without changing its context(follow open/close principle).
+ - Change behavior at runtime.
+
+
+ Different from State design pattern
+ State pattern - focus on state transitions, the context itself changes behavour and it is ofter tied with the idea of stateful logic.
+
+ Strategy pattern - Focus on interchangable algorithm, the algorithm is injected into the context and it is more about stateless behavior swap.
+
+ In summary:
+ State pattern - The same method behaves differently based on the object's current state.
+ Strategy pattern - The same task can be done using different algorithms that can be swapped in/out.
+ */
